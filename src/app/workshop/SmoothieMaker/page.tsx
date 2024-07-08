aa4914f62a5c15e0408f467a03c2dcf1ff1e1d59
Emil0309 <71798519+Emil0309@users.noreply.github.com>
2024-07-08 10:01:12 -0400

    Este es el sexto commit de emil 8/7/2024

--- src/app/workshop/SmoothieMaker/page.tsx
+++ src/app/workshop/SmoothieMaker/page.tsx
@@ -1,6 +1,16 @@
 "use client"
 import Navbar from '@/components/front-end/Navbar'
 import React from 'react'
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
 
 const SmoothieMaker = () => {
   return (
@@ -8,7 +18,24 @@ const SmoothieMaker = () => {
       <Navbar setShowCart={function (value: React.SetStateAction<boolean>): void {
         throw new Error('Function not implemented.')
       } }/>
-      <h1>Smoothie Maker</h1>
+
+      <div className='w-full h-full'>
+      <DropdownMenu>
+  <DropdownMenuTrigger>Open</DropdownMenuTrigger>
+  <DropdownMenuContent>
+    <DropdownMenuLabel>My Account</DropdownMenuLabel>
+    <DropdownMenuSeparator />
+    <DropdownMenuItem>Profile</DropdownMenuItem>
+    <DropdownMenuItem>Billing</DropdownMenuItem>
+    <DropdownMenuItem>Team</DropdownMenuItem>
+    <DropdownMenuItem>Subscription</DropdownMenuItem>
+  </DropdownMenuContent>
+</DropdownMenu>
+
+      </div>
+
+
+
     </div>
   )
 }
